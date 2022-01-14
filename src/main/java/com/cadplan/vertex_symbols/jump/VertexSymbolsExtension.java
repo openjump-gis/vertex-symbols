cc97f3b6f433ca8ecf99552e345187b29a722fbc
ede <edgar.soldin@web.de>
2022-01-14 23:53:57 +0100

    cleanup

--- src/main/java/com/cadplan/vertex_symbols/jump/VertexSymbolsExtension.java
+++ src/main/java/com/cadplan/vertex_symbols/jump/VertexSymbolsExtension.java
@@ -1,23 +1,19 @@
 package com.cadplan.vertex_symbols.jump;
 
 import java.io.File;
-import java.net.URL;
 
 import com.cadplan.vertex_symbols.jump.plugins.StylerMenuPlugIn;
 import com.vividsolutions.jump.I18N;
-import com.vividsolutions.jump.util.FileUtil;
 import com.vividsolutions.jump.workbench.Logger;
 import com.vividsolutions.jump.workbench.WorkbenchContext;
 import com.vividsolutions.jump.workbench.plugin.Extension;
 import com.vividsolutions.jump.workbench.plugin.PlugInContext;
-import com.vividsolutions.jump.workbench.plugin.PlugInManager;
 
 
 public class VertexSymbolsExtension extends Extension {
 
 	private static final I18N I18N = com.vividsolutions.jump.I18N.getInstance("com.cadplan.vertex_symbols");
-	private File symbolFolder = null;
-	
+
 	@Override
 	public void configure(PlugInContext context) throws Exception {
 		//	new VertexSymbolsPlugIn().initialize(context);
